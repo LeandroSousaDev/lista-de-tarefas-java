c7ab5d95c9375dfcfa25e83318e892a6f558afe7
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-07-18 17:36:54 -0300

    confiração repository e rota creação de usuario

--- src/main/java/br/com/leandrosousa/todolist/user/UserController.java
+++ src/main/java/br/com/leandrosousa/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.leandrosousa.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("Usuario ja cadastrado");
+            return null;
+        }
+
+        var usuarioCriado = this.userRepository.save(userModel);
+        return usuarioCriado;
+
     }
 
 }
--- src/main/java/br/com/leandrosousa/todolist/user/UserModel.java
+++ src/main/java/br/com/leandrosousa/todolist/user/UserModel.java
@@ -1,25 +1,27 @@
 package br.com.leandrosousa.todolist.user;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "tb_users")
+@Table(name = "tb_users_2")
 public class UserModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
